6b94ca4ee6670c1c9ef7865a96e82fa8b81cf6bb
luisguilhermerosa <luisguigo2@gmail.com>
2025-09-03 01:07:39 +0000

    02/09

--- projeto_kart/src/app/contador/conta/conta.ts
+++ projeto_kart/src/app/contador/conta/conta.ts
@@ -18,4 +18,110 @@ diminuircontador() {
   this.valor--;
 }
 
+valor2: number=0;
+
+aumentarcontador2() {
+  this.valor2++;
+}
+
+diminuircontador2() {
+  this.valor2--;
+}
+
+valor3: number=0;
+
+aumentarcontador3() {
+  this.valor3++;
+}
+
+diminuircontador3() {
+  this.valor3--;
+}
+
+valor4: number=0;
+
+aumentarcontador4() {
+  this.valor4++;
+}
+
+diminuircontador4() {
+  this.valor4--;
+}
+
+valor5: number=0;
+
+aumentarcontador5() {
+  this.valor5++;
+}
+
+diminuircontador5() {
+  this.valor5--;
+}
+
+
+valor6: number=0;
+
+aumentarcontador6() {
+  this.valor6++;
+}
+
+diminuircontador6() {
+  this.valor6--;
+}
+
+valor7: number=0;
+
+aumentarcontador7() {
+  this.valor7++;
+}
+
+diminuircontador7() {
+  this.valor7--;
+}
+
+
+valor8: number=0;
+
+aumentarcontador8() {
+  this.valor8++;
+}
+
+diminuircontador8() {
+  this.valor8--;
+}
+
+valor9: number=0;
+
+aumentarcontador9() {
+  this.valor9++;
+}
+
+diminuircontador9() {
+  this.valor9--;
+}
+
+valor10: number=0;
+
+aumentarcontador10() {
+  this.valor10++;
+}
+
+diminuircontador10() {
+  this.valor10--;
+}
+
+valor11: number=0;
+
+aumentarcontador11() {
+  this.valor11++;
+}
+
+diminuircontador11() {
+  this.valor11--;
+}
+
+
+
+
+
 }
